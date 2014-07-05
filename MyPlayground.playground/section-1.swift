e945ba6777040879da06923bd440ec18b04772e5
yangboz <YoungWelle@gmail.com>
2014-07-05 09:21:42 +0800

    ADDED:MyPlayGround file

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,14 @@
+let tutorialTeam:Int = 56
+let editorialTeam = 23
+var totalTeam = tutorialTeam + editorialTeam
+//totalTeam +=1
+let priceInferred:Double = 19.99
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+if onSaleInferred{
+    println("\(nameInferred) on sale for \(priceInferred)!")
+}else{
+    println("\(nameInferred) at regular price: \(priceInferred)")
+}
