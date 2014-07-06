36d46ce80c0067d23e505d901f39baab37014d0a
yangboz <YoungWelle@gmail.com>
2014-07-06 13:45:23 +0800

    ADDED:CookieCrash sample code

---
+++ CookieCrunch/CookieCrunch/GameScene.swift
@@ -0,0 +1,12 @@
+import SpriteKit
+
+class GameScene: SKScene {
+    init(size: CGSize) {
+        super.init(size: size)
+        
+        anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        let background = SKSpriteNode(imageNamed: "Background")
+        addChild(background)
+    }
+}
